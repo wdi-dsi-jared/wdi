f2e9ce8f8d3390b84e5cedc680c5720ea0b6d1e4
Paris Leach <parisleach@gmail.com>
2016-12-07 13:01:40 -0500

    fun changes

--- src/Components/DummyGraph/DummyGraph.jsx
+++ src/Components/DummyGraph/DummyGraph.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+const Chart = require('react-d3-core').Chart;
+const LineChart = require('react-d3-basic').LineChart;
 
 export default class DummyGraph extends Component {
 
   render() {
     return(
       <div>
-        <h3>dummy graph goes here</h3>
+      <Chart
+      title={'User Sample'}
+      width={700}
+      height={300}
+      margins= {{left: 100, right: 100, top: 50, bottom: 50}}
+      >
+      <LineChart
+        margins= {{left: 100, right: 100, top: 50, bottom: 50}}
+        title={'User Sample'}
+        data={this.props.data}
+        width={700}
+        height={300}
+        chartSeries={[
+      {
+        field: 'BMI',
+        name: 'BMI',
+        color: '#ff7f0e'
+      }
+    ]}
+        x={(d) => d.index}
+      />
+      </Chart>
         <button onClick={this.props.getDummyData}>click to get data</button>
       </div>
     )
