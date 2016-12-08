e8202b3e4c9eb84cff46e5f05b0f4eb587561daf
Daniel Pease <daniel.j.pease@gmail.com>
2016-12-07 21:00:01 -0500

    successfully pinged route

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # wdi
+
+(2016, 11, 26, 6, 0)
+year month day hour minute (no seconds)
--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Graph from '../Graph/Graph.jsx';
 import { BarChart } from 'react-d3';
 import Search from '../Search/Search.jsx';
 
@@ -8,51 +7,64 @@ export default class App extends Component {
     super();
 
     this.state = {
-      data: '',
+      data: [
+        {
+          "name": "Predicted Congestion",
+          "values": [
+            { "x": 1, "y":  91},
+            { "x": 2, "y":  101},
+            { "x": 3, "y":  111},
+            { "x": 4, "y":  121},
+            { "x": 5, "y":  131},
+            { "x": 6, "y":  141},
+            { "x": 7, "y":  151},
+            { "x": 8, "y":  161},
+            { "x": 9, "y":  171},
+            { "x": 10, "y":  181},
+            { "x": 11, "y":  171},
+            { "x": 12, "y":  161},
+            { "x": 13, "y":  151},
+            { "x": 14, "y":  161},
+            { "x": 15, "y":  171},
+            { "x": 16, "y":  181},
+            { "x": 17, "y":  191},
+            { "x": 18, "y":  201},
+            { "x": 19, "y":  201},
+            { "x": 20, "y":  211},
+            { "x": 21, "y":  151},
+            { "x": 22, "y":  131},
+            { "x": 23, "y":  81},
+          ]
+        }
+      ],
     }
-    this.barData = [
-      //this will be replaced by the data we pull from our fetch (see above)
-      {
-        "name": "Series A",
-        "values": [
-          { "x": 1, "y":  91},
-        ]
-      },
-      {
-        "name": "Series B",
-        "values": [
-          { "x": 1, "y":  91},
-        ]
-      }
-    ];
   }
 
   getDummyData() {
     console.log('getDummyData invoked!')
-    this.setState({
-      data: this.barData
-    })
     console.log('getDummyData complete!')
   }
 
   graph() {
     return this.state.data ? ( <div>
-        <h1>hi!</h1>
-
-        <Graph
-          data={this.state.placeholder}
-          getDummyData={this.getDummyData.bind(this)}
-        />
         <BarChart
-          data={this.barData}
+          data={this.state.data}
           width={500}
           height={200}
           fill={'#3182bd'}
-          title='Bar Chart'
           yAxisLabel='people'
           xAxisLabel='time'
         />
       </div> ) : <div/>;
+  }
+
+  testPing() {
+    console.log('running test ping')
+    return fetch('http://ec2-54-89-253-54.compute-1.amazonaws.com/myapp/make_it_happen')
+    .then(r => r.json())
+    .then((data) => {
+      console.log(data)
+    })
   }
 
   render() {
@@ -61,6 +73,7 @@ export default class App extends Component {
       <div>
         <Search getDummyData={this.getDummyData.bind(this)}/>
         {this.graph()}
+        <button onClick={this.testPing}>Test</button>
       </div>
     )
   }
